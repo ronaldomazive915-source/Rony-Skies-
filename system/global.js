1e1e4d01974c6822418bb379b463a3d6a3d68b7e
ronaldomazive915-source <ronaldomazive915@gmail.com>
2025-12-06 20:53:13 +0200

    Add files via upload

---
+++ system/global.js
@@ -0,0 +1,16 @@
+//  Base YT : @YASSxOFC 
+//  Recode By Kyy famofc
+//  90% Fitur By Kyy
+
+
+require("./module")
+require("../settings")
+require("./exif")
+
+let file = require.resolve(__filename) 
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.cyan("File Update => "), chalk.cyan.bgBlue.bold(`${__filename}`))
+delete require.cache[file]
+require(file)
+})
